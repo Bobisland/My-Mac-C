dad730218e92961c65b23231fbe09eb6b48cc87e
Bobisland <2132524969@qq.com>
2023-10-22 21:20:19 +0800

    test

--- test.c
+++ test.c
@@ -3,7 +3,15 @@ int main()
 {
 	printf("Hello World");
 	int a;
+	int b = 4;
+	printf("%d",b);
+	printf("Hello World");
+		
 	a = 1;
+	printf("Hello World");
+	
 	printf("%d",a);
+	
 	return 0;
+
 }
